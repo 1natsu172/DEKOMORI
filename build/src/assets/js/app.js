24ee5966fe44093897235009842675123a1d1691
1natsu <1natsummer+github@gmail.com>
2017-08-28 19:41:16 +0900

    fix pug index

--- build/src/assets/js/app.js
+++ build/src/assets/js/app.js
@@ -13,13 +13,13 @@ document.addEventListener('DOMContentLoaded', () => {
   // Prevent reverse tabnabbing based phishing attacks that take advantage of _blank targets.
   Blankshield(document.querySelectorAll('a[target=_blank]'))
 
-  // Check behavior//*
-  console.log('jQuery:', $.fn) // OK
-  console.log('modernizr:', Modernizr) // OK
-  console.log('ImagesLoaded:', ImagesLoaded) // OK
-  console.log('WebFont:', WebFont.load) // OK
-  console.log('matchHeight:', $.fn.matchHeight) // OK
+  // Check behavior//
+  console.log('jQuery:', $.fn)
+  console.log('modernizr:', Modernizr)
+  console.log('ImagesLoaded:', ImagesLoaded)
+  console.log('WebFont:', WebFont.load)
+  console.log('matchHeight:', $.fn.matchHeight)
   console.log('process.env.NODE_ENV:', process.env.NODE_ENV)
   sum(1, 2) // Webpack TreeShaking test
-  $('body').css('background', '#EECE69')
+  $('body').css('background', '#7ca4ff') // can use jQuery
 })
