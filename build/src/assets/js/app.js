adfb61275c3216ab150509bea0c3dbf385f8153f
1natsu <1natsummer+github@gmail.com>
2017-09-27 12:42:32 +0900

    change JS import
    Camel case to lower case

--- build/src/assets/js/app.js
+++ build/src/assets/js/app.js
@@ -1,6 +1,6 @@
-import Blankshield from 'blankshield'
-import Modernizr from 'modernizr'
-import WebFont from 'webfontloader'
+import blankshield from 'blankshield'
+import modernizr from 'modernizr'
+import webfontloader from 'webfontloader'
 import ImagesLoaded from 'imagesloaded'
 import 'jquery-match-height'
 import 'svgxuse' // svg use polyfill
@@ -11,13 +11,19 @@ import { sum } from 'math'
 // ✍️
 document.addEventListener('DOMContentLoaded', () => {
   // Prevent reverse tabnabbing based phishing attacks that take advantage of _blank targets.
-  Blankshield(document.querySelectorAll('a[target=_blank]'))
+  const getBlankAnchors = document.querySelectorAll('a[target=_blank]')
+  for (var i = 0; i < getBlankAnchors.length; i++) {
+    getBlankAnchors[i].addEventListener('click', function(e) {
+      e.stopImmediatePropagation()
+    })
+  }
+  blankshield(getBlankAnchors)
 
   // Check behavior//
   console.log('jQuery:', $.fn)
-  console.log('modernizr:', Modernizr)
+  console.log('modernizr:', modernizr)
   console.log('ImagesLoaded:', ImagesLoaded)
-  console.log('WebFont:', WebFont.load)
+  console.log('WebFont:', webfontloader.load)
   console.log('matchHeight:', $.fn.matchHeight)
   console.log('process.env.NODE_ENV:', process.env.NODE_ENV)
   sum(1, 2) // Webpack TreeShaking test
--- build/src/assets/js/index.js
+++ build/src/assets/js/index.js
@@ -1,12 +1,3 @@
-import Modernizr from 'modernizr'
-import WebFont from 'webfontloader'
-import ImagesLoaded from 'imagesloaded'
-import 'jquery-match-height'
-import 'svgxuse'
-import { TweenLite, TweenMax, TimelineMax, Elastic } from 'gsap'
-import foo from 'foo'
-import { sum } from 'math'
-
 // ✍️
 document.addEventListener('DOMContentLoaded', () => {
   console.log('index~')
